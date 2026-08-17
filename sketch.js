2afeacd4058def6bba36a0d6e17d368f69d04228
Ryan Eakins <ryanjeffeakins@gmail.com>
2026-08-17 17:13:15 -0400

    Update final code for HTML, CSS, and JS and push to completion

--- sketch.js
+++ sketch.js
@@ -1,60 +1,73 @@
-const btnBlack = document.getElementById("black");
-const btnReset = document.getElementById("reset");
-const btnPrompt = document.getElementById("prompt");
+const container = document.getElementById("sketch-pad-container");
+const clearButton = document.getElementById("clearButton");
+const gridButton = document.getElementById("gridButton");
+const sketchPad = document.getElementById("sketch-pad");
+const blackBrush = document.getElementById("black-Brush");
+const colorfulBrush = document.getElementById("colorful-Brush");
 
-let currentColor = 'black';
+//Grid Button
+gridButton.addEventListener("click", function(){
+    const amount = prompt("What amount of squares per side are you wanting?", "26");
 
-const colors = {
-    black: "my-color-class"
-};
+    appendNewSketchPad(createGrid(amount));
+});
 
-//Create 16x16 Grid for Function with Container
-function createGrid(size) {
-    const container = document.getElementById("grid-container")
-
-    for (let i = 0; i < size; i++) {
-        let column = document.createElement("div");
-        column.classList.add("column");
-
-        for (let j = 0; j < size; j++) {
-            let cell = document.createElement("div");
-            cell.classList.add("cell-row");
-
-            cell.addEventListener('mouseover', e => {
-                e.target.classList.add(colors[currentColor]);
-            });
-        
-            column.appendChild(cell);
-        }
-
-        container.appendChild(column);
-    }
-}
-
-createGrid(16);
-
-//Add Reset Button to Screen
-btnReset.classList.add("reset");
-btnReset.textContent = "Reset";
-
-//Event Listener for Reset Button
-btnReset.addEventListener("click", () => {
-    createGrid.forEach((size) => {
-        size.style.backgroundColor = "grey";
+//Reset Button
+clearButton.addEventListener("click", function(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.style.background = "grey";
     });
 });
 
-createGrid(16);
-
-//Choose Number of Squares
-let numberButton = document.getElementById("prompt");
-numberButton.addEventListener("click", () => {
-    let enterNumber;
-    while (isNaN(enterNumber) || enterNumber > 100 || enterNumber < 0) {
-        enterNumber = Number(window.prompt("Choose a number between 0 and 100"));
-    }
-
-    //Clear Squares
-    container.innerHTML = "";
-    createGrid(enterNumber);
+//Black Brush
+blackBrush.addEventListener("click", function(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", function(e){
+            e.target.style.backgroundColor = "black";
+        });
+    });
 });
+
+//Colorful Brush
+colorfulBrush.addEventListener("click", function() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", function(e){
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            e.target.style.backgroundColor = `rgb(${r} ${g} ${b})`;
+        });
+    });
+});
+
+//Container Function
+function createGrid(num) {
+    const sketchPad = document.createElement("div");
+    sketchPad.classList.add("sketch-pad");
+    console.log(container)
+    for (let i = 0; i < num * num; i++) {
+        let newGridCreated = document.createElement("div");
+
+        newGridCreated.classList.add("square");
+        newGridCreated.style.backgroundColor = "grey";
+
+        sketchPad.appendChild(newGridCreated);
+        document.documentElement.style.setProperty("--squares-per-side",num);
+    }
+    return sketchPad;
+}
+
+//Sketch Pad Function
+function appendNewSketchPad(sketchPad) {
+    const oldSketchPad = document.querySelector(".sketch-pad");
+
+    if (oldSketchPad) {
+        container.removeChild(oldSketchPad);
+    }
+    container.appendChild(sketchPad);
+}
+
+appendNewSketchPad(createGrid(24));
